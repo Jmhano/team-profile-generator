fb0bd9d220f9f1697756c2abab57d9cb80a74abb
Jmhano <jmhano@gmail.com>
2023-01-12 13:25:54 -0500

    able to return employee object test now

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,29 +1,29 @@
 const Employee = require("../lib/Employee.js");
 
 test("creates an employee object", () => {
-  const employee = new Employee("Jen");
+  const employee = new Employee("Jen", "4", "admin@admin.com");
 
-  expect(employee.name).toBe("name");
-  expect(employee.id).toEqual(expect.any(id));
-  expect(employee.email).toBe(expect.any(email));
+  expect(employee.name).toBe("Jen");
+  expect(employee.id).toBe("4");
+  expect(employee.email).toBe( "admin@admin.com");
 });
 
-test("get emoloyee name as a new object", () => {
-  const emoloyee = new emoloyee("jen");
-  expect(employee.getName).toHaveProperty("name");
-});
+// test("get emoloyee name as a new object", () => {
+//   const emoloyee = new emoloyee("jen");
+//   expect(employee.getName).toHaveProperty("name");
+// });
 
-test("get emoloyee id as a new object", () => {
-  const emoloyee = new emoloyee("jen");
-  expect(employee.getID).toHaveProperty("id");
-});
+// test("get emoloyee id as a new object", () => {
+//   const emoloyee = new emoloyee("jen");
+//   expect(employee.getID).toHaveProperty("id");
+// });
 
-test("get emoloyee email as a new object", () => {
-  const emoloyee = new emoloyee("jen");
-  expect(employee.getEmail).toHaveProperty("email");
-});
+// test("get emoloyee email as a new object", () => {
+//   const emoloyee = new emoloyee("jen");
+//   expect(employee.getEmail).toHaveProperty("email");
+// });
 
-test("get emoloyee role as a new object", () => {
-  const emoloyee = new emoloyee("jen");
-  expect(employee.getRole).toHaveProperty("role");
-});
+// test("get emoloyee role as a new object", () => {
+//   const emoloyee = new emoloyee("jen");
+//   expect(employee.getRole).toHaveProperty("role");
+// });
